c67cc2ae86723099d340a632ec7882e0a643cc8e
Olly Swanson <olly.swanson95@gmail.com>
2022-08-26 10:44:49 +0100

    Register `AddressesHttpGateway` and return mock

--- HousingManagementSystemApi/Gateways/AddressesHttpGateway.cs
+++ HousingManagementSystemApi/Gateways/AddressesHttpGateway.cs
@@ -8,29 +8,48 @@ namespace HousingManagementSystemApi.Gateways
 {
     public class AddressesHttpGateway : IAddressesGateway
     {
-        private readonly HttpClient httpClient;
-        private readonly string addressesApiUrl;
-        private readonly string addressesApiKey;
+        // private readonly HttpClient httpClient;
+        // private readonly string addressesApiUrl;
+        // private readonly string addressesApiKey;
 
-        public AddressesHttpGateway(HttpClient httpClient, string addressesApiUrl, string addressesApiKey)
-        {
-            this.httpClient = httpClient;
-            this.addressesApiUrl = addressesApiUrl;
-            this.addressesApiKey = addressesApiKey;
-        }
+        // public AddressesHttpGateway(HttpClient httpClient, string addressesApiUrl, string addressesApiKey)
+        // {
+        //     this.httpClient = httpClient;
+        //     this.addressesApiUrl = addressesApiUrl;
+        //     this.addressesApiKey = addressesApiKey;
+        // }
 
         public async Task<IEnumerable<PropertyAddress>> SearchByPostcode(string postcode)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get,
-                $"{addressesApiUrl}/address?postcode={postcode}");
-            request.Headers.Add("X-API-Key", addressesApiKey);
-            var response = await httpClient.SendAsync(request);
-
-            var data = new List<PropertyAddress>();
-            if (response.StatusCode == HttpStatusCode.OK)
+            // var request = new HttpRequestMessage(HttpMethod.Get,
+            //     $"{addressesApiUrl}/address?postcode={postcode}");
+            // request.Headers.Add("X-API-Key", addressesApiKey);
+            // var response = await httpClient.SendAsync(request);
+            //
+            // var data = new List<PropertyAddress>();
+            // if (response.StatusCode == HttpStatusCode.OK)
+            // {
+            //     data = await response.Content.ReadFromJsonAsync<List<PropertyAddress>>();
+            // }
+            var data = new List<PropertyAddress>
             {
-                data = await response.Content.ReadFromJsonAsync<List<PropertyAddress>>();
-            }
+               new PropertyAddress()
+               {
+                   PostalCode = "LN1 3AT",
+                   StreetName = "Diagon Alley",
+                   Country = CountryCode.AD,
+                   Reference = new Reference()
+                   {
+                       AllocatedBy = "X",
+                       Description = "Y"
+                   },
+                   AddressLine = new List<string>
+                   {
+                       "AddressLine"
+                   },
+                   CityName = "London"
+               }
+            };
 
             return data;
         }
